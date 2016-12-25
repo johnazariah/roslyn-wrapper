80a48f2892cbfa0a880d1ac215a881cccf83c292
John Azariah <john.azariah@gmail.com>
2016-12-25 13:35:24 -0800

    Add tests for Namespace

---
+++ tests/BrightSword.RoslynWrapper.Tests/NamespaceTests.fs
@@ -0,0 +1,68 @@
+﻿namespace BrightSword.RoslynWrapper.Tests
+
+open NUnit.Framework
+
+open BrightSword.RoslynWrapper
+
+module NamespaceTests = 
+
+    [<Test>]
+    let ``namespace: empty``() =
+        let n = 
+            ``namespace`` "Foo"
+                ``{`` 
+                    []
+                    []
+                ``}``
+        let actual = to_namespace_code n
+        let expected = @"namespace Foo
+{
+    using System;
+}"
+        are_equal expected actual
+
+    [<Test>]
+    let ``namespace: with usings``() =
+        let n = 
+            ``namespace`` "Foo"
+                ``{`` 
+                    [ "System.Collections" ]
+                    []
+                ``}``
+        let actual = to_namespace_code n
+        let expected = @"namespace Foo
+{
+    using System;
+    using System.Collections;
+}"
+        are_equal expected actual
+
+    [<Test>]
+    let ``namespace: with usings and classes``() =
+        let c =
+            ``class`` "C" ``<<`` [] ``>>``
+                ``:`` None ``,`` []
+                [``public``]
+                ``{``
+                    []
+                ``}``
+        let n = 
+            ``namespace`` "Foo"
+                ``{`` 
+                    [ "System.Collections" ]
+                    [ c ]
+                ``}``
+        let actual = to_namespace_code n
+        let expected = @"namespace Foo
+{
+    using System;
+    using System.Collections;
+
+    public class C
+    {
+    }
+}"
+        are_equal expected actual
+
+
+
