1ae7a8d640fc6baf9a7539e9a437cf6ec0655768
John Azariah <john.azariah@gmail.com>
2016-12-31 11:59:57 -0800

    Add operators, statements and expressions to support the C# SumTypes patterns

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,5 @@
-#### 1.0.0 - December 28 2016
+#### 1.0.1 - December 31 2016
+* Added more expressions
 * Initial set of code, tests & docs
 * Changed name from fsharp-project-scaffold to BrightSword.RoslynWrapper
 * Initial release
---
+++ tests/BrightSword.RoslynWrapper.Tests/EqualityOperatorTests.fs
@@ -0,0 +1,42 @@
+﻿namespace BrightSword.RoslynWrapper.Tests
+
+open NUnit.Framework
+
+open BrightSword.RoslynWrapper
+
+module EqualityOperatorTests =
+    [<Test>]
+    let ``equality operator: ==`` () =
+        let m = 
+            ``operator ==`` ("left", "right", ``type`` "string")
+                (``=>`` ``true``)
+
+        let actual = to_class_members_code [m]
+        let expected = @"namespace N
+{
+    using System;
+
+    public class C
+    {
+        public static bool operator ==(string left, string right) => true;
+    }
+}"
+        are_equal expected actual
+
+    [<Test>]
+    let ``equality operator: !=`` () =
+        let m = 
+            ``operator !=`` ("left", "right", ``type`` "string")
+                (``=>`` ``true``)
+
+        let actual = to_class_members_code [m]
+        let expected = @"namespace N
+{
+    using System;
+
+    public class C
+    {
+        public static bool operator !=(string left, string right) => true;
+    }
+}"
+        are_equal expected actual
